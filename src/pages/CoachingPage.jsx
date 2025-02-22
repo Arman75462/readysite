e53324c2fc7a8888168abdea8a9540d9db58a619
Arman <armangulistani@gmail.com>
2025-02-22 16:20:00 -0500

    Change coaching page and equipments page title
    Changed it from <En construction..> to <Bientôt disponible> because the former title would overlap the page and make horizontal scrolling possible.

--- src/pages/CoachingPage.jsx
+++ src/pages/CoachingPage.jsx
@@ -3,7 +3,7 @@ import "./styles/CoachingPage.css";
 export default function CoachingPage() {
   return (
     <section className="CoachingPage page-appearance-animation">
-      <h2 className="CoachingPage__title">En construction...</h2>
+      <h2 className="CoachingPage__title">Bientôt disponible</h2>
     </section>
   );
 }
--- src/pages/EquipmentsPage.jsx
+++ src/pages/EquipmentsPage.jsx
@@ -3,7 +3,7 @@ import "./styles/EquipmentsPage.css";
 export default function EquipmentsPage() {
   return (
     <section className="EquipmentsPage page-appearance-animation">
-      <h2 className="CoachingPage__title">En construction...</h2>
+      <h2 className="CoachingPage__title">Bientôt disponible</h2>
     </section>
   );
 }
